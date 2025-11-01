f48f5cd8cdd5c9cd3b387bb7a931382e51276d96
kaw <kaw>
2025-11-01 15:45:16 +0000

    adjustments for 7.8 (arm64)

--- lib/makebootarc_arm64.sh
+++ lib/makebootarc_arm64.sh
@@ -13,9 +13,18 @@ cp -p /usr/local/share/raspberrypi-firmware/boot/bcm2710-rpi-2-b.dtb bcm2710-rpi
 cp -p /usr/local/share/raspberrypi-firmware/boot/bcm2710-rpi-3-b-plus.dtb bcm2710-rpi-3-b-plus.dtb
 cp -p /usr/local/share/raspberrypi-firmware/boot/bcm2710-rpi-3-b.dtb bcm2710-rpi-3-b.dtb
 cp -p /usr/local/share/raspberrypi-firmware/boot/bcm2710-rpi-cm3.dtb bcm2710-rpi-cm3.dtb
+cp -p /usr/local/share/raspberrypi-firmware/boot/bcm2710-rpi-zero-2-w.dtb bcm2710-rpi-zero-2-w.dtb
 cp -p /usr/local/share/raspberrypi-firmware/boot/bcm2711-rpi-4-b.dtb bcm2711-rpi-4-b.dtb
 cp -p /usr/local/share/raspberrypi-firmware/boot/bcm2711-rpi-400.dtb bcm2711-rpi-400.dtb
 cp -p /usr/local/share/raspberrypi-firmware/boot/bcm2711-rpi-cm4.dtb bcm2711-rpi-cm4.dtb
+cp -p /usr/local/share/raspberrypi-firmware/boot/bcm2712-d-rpi-5-b.dtb bcm2712-d-rpi-5-b.dtb
+cp -p /usr/local/share/raspberrypi-firmware/boot/bcm2712-rpi-5-b.dtb bcm2712-rpi-5-b.dtb
+cp -p /usr/local/share/raspberrypi-firmware/boot/bcm2712-rpi-500.dtb bcm2712-rpi-500.dtb
+cp -p /usr/local/share/raspberrypi-firmware/boot/bcm2712-rpi-cm5-cm4io.dtb bcm2712-rpi-cm5-cm4io.dtb
+cp -p /usr/local/share/raspberrypi-firmware/boot/bcm2712-rpi-cm5-cm5io.dtb bcm2712-rpi-cm5-cm5io.dtb
+cp -p /usr/local/share/raspberrypi-firmware/boot/bcm2712-rpi-cm5l-cm4io.dtb bcm2712-rpi-cm5l-cm4io.dtb
+cp -p /usr/local/share/raspberrypi-firmware/boot/bcm2712-rpi-cm5l-cm5io.dtb bcm2712-rpi-cm5l-cm5io.dtb
+cp -p /usr/local/share/raspberrypi-firmware/boot/bcm2712d0-rpi-5-b.dtb bcm2712d0-rpi-5-b.dtb
 cp -p /usr/local/share/raspberrypi-firmware/boot/bootcode.bin bootcode.bin
 
 # config.txt not found: replace this with a command line to create config.txt
@@ -32,16 +41,17 @@ cp /usr/mdec/BOOTAA64.EFI efi/openbsd/bootaa64.efi
 
 cp -p /usr/local/share/raspberrypi-firmware/boot/fixup.dat fixup.dat
 cp -p /usr/local/share/raspberrypi-firmware/boot/fixup4.dat fixup4.dat
+cp -p /usr/local/share/raspberrypi-firmware/boot/overlays/bcm2712d0.dtbo overlays/bcm2712d0.dtbo
 cp -p /usr/local/share/raspberrypi-firmware/boot/overlays/disable-bt.dtbo overlays/disable-bt.dtbo
 cp -p /usr/local/share/raspberrypi-firmware/boot/start.elf start.elf
 cp -p /usr/local/share/raspberrypi-firmware/boot/start4.elf start4.elf
-
-# found u-boot.bin in multiple paths
-# please select (uncomment) from following lines
-# dup: cp -p /usr/local/share/u-boot/mvebu_espressobin-88f3720/u-boot.bin u-boot.bin
-# dup: cp -p /usr/local/share/u-boot/mvebu_mcbin-88f8040/u-boot.bin u-boot.bin
-# dup: cp -p /usr/local/share/u-boot/qemu_arm64/u-boot.bin u-boot.bin
 cp -p /usr/local/share/u-boot/rpi_arm64/u-boot.bin u-boot.bin
+
+# ubootefi.var not found: replace this with a command line to create ubootefi.var
+if mount /dev/sd0i /mnt; then
+    cp /mnt/ubootefi.var .
+    umount /dev/sd0i
+fi
 
 tar cvzf '../bootstuff.arm64.tar.gz' .
 cd ..
